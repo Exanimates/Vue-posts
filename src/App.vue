444a23c75f512ddd5b439e18973db5e9565649be
Igor Maxsimov <maxsimovwork@gmail.com>
2022-09-01 13:01:12 +0700

    Add post render and filtered.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <input type="text" @input="event => searchText = event.target.value"/>
+  <PostComp v-for="post in filteredPosts" :key="post.id" :post="post"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PostComp from './components/PostComp.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PostComp
+  },
+
+  data() {
+    return {
+      users: [],
+      posts: [],
+      searchText: ''
+    }
+  },
+
+  computed: {
+    filteredPosts: function () {
+      if (this.searchText) return this.posts.filter(post => post.username.startsWith(this.searchText))
+
+      return this.posts;
+    }
+  },
+
+  methods: {
+    postUsersMap() {
+      this.posts = this.posts.map((post)=>{
+          let userById = this.users.find(user=> user.id === post.userId)
+          if(userById.name) {
+            post.username = userById.name;
+          }
+          return post;
+      })
+    }
+  },
+
+  async created() {
+    this.posts = await (await fetch("https://jsonplaceholder.typicode.com/posts")).json();
+    this.users = await (await fetch("https://jsonplaceholder.typicode.com/users")).json();
+    this.postUsersMap();
   }
+
+
 }
 </script>
 
---
+++ src/components/PostComp.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h1>{{ post.title }}</h1>
+    <div>{{ post.title }}</div>
+    <div>{{ post.username }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PostComp',
+  props: {
+    post: Object
+  }
+}
+</script>
+
+<style scoped>
+</style>
